6e4a08871242eaa49984c4469fa333ef337b87fb
wp <118538385+XSeellAlax@users.noreply.github.com>
2024-06-29 23:01:35 +0800

    version 3.0

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "utils.h"
-
+#include <cstdio>
 int main() {
     std::string line;
+    std::cout << "\033[1;33m-------------------------------------------------\n";
+    std::cout << "\033[1;33m|         This project is by 王鹏 王修悟 :)         |\n";
+    std::cout << "\033[1;33m-------------------------------------------------\n";
     while (true) {
-        std::cout << "myshell> ";
+        std::cout << "\033myshell> ";
 
         if (!std::getline(std::cin, line) || line == "exit") {
         
